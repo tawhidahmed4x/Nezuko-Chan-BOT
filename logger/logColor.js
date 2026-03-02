ae087d00f5977f9e294b8320be15c651988876d4
Mthacks420 <132431178+Mthacks420@users.noreply.github.com>
2026-03-02 07:38:45 +0600

    Update logColor.js

--- logger/logColor.js
+++ logger/logColor.js
@@ -1,2 +1,12 @@
 const { colors } = require('../func/colors.js');
-module.exports = (color, message) => console.log(colors.hex(color, message));
+
+module.exports = (color, message) => {
+	// যদি শুধু মেসেজ দেওয়া হয় কালার না থাকে, তবে ডিফল্ট একটা কিউট কালার নেবে
+	if (!message) {
+		message = color;
+		color = "#ff69b4"; // Nezuko Pink Color
+	}
+	
+	const prefix = "🌸 [NEZUKO] => ";
+	console.log(colors.hex(color, `${prefix}${message}`));
+};
