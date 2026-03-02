622264b1073eda9e3b60a2e1934ac8822e721b51
tawhidahmed4x <noobnahid19@gmail.com>
2026-03-02 23:28:35 +0000

    Update loadData.js

--- bot/login/loadData.js
+++ bot/login/loadData.js
@@ -5,15 +5,11 @@ module.exports = async function (api, createLine) {
     console.log(colors.hex("#00FFFF")(" 🔄 [DATABASE] TawHid_Bbz, memories loading..."));
     
     try {
-        // Main directory theke database/controller/index.js load kora hocche
         const controllerPath = path.join(process.cwd(), 'database/controller/index.js');
         const controller = require(controllerPath);
-        
-        // Controller function call
         await controller(api);
         
-        console.log(colors.hex("#33FF33")(` ✅ [THREADS] ${global.db.allThreadData.length} Groups Loaded.`));
-        console.log(colors.hex("#33FF33")(` ✅ [USERS] ${global.db.allUserData.length} Users Loaded.`));
+        log.info("DATABASE", "Threads and Users data loaded successfully.");
     } catch (err) {
         log.err('DATABASE', "Error during data loading:", err);
         throw err;
