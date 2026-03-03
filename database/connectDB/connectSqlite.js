dea90c81fa96ac3e784df0fd2ddbd7087a9aae49
tawhidahmed4x <noobnahid19@gmail.com>
2026-03-03 07:28:08 +0600

    Update connectSqlite.js

--- database/connectDB/connectSqlite.js
+++ database/connectDB/connectSqlite.js
@@ -1,31 +1,24 @@
-const { Sequelize } = require("sequelize");
-const path = require("path");
-
 module.exports = async function () {
-    const sqlitePath = path.join(__dirname, "..", "data/data.sqlite");
-    const sequelize = new Sequelize({
-        dialect: "sqlite",
-        host: sqlitePath,
-        logging: false
-    });
+	const { Sequelize } = require("sequelize");
+	const path = __dirname + "/../data/data.sqlite";
+	const sequelize = new Sequelize({
+		dialect: "sqlite",
+		host: path,
+		logging: false
+	});
 
-    try {
-        const threadModel = require("../models/sqlite/thread.js")(sequelize);
-        const userModel = require("../models/sqlite/user.js")(sequelize);
-        const dashBoardModel = require("../models/sqlite/userDashBoard.js")(sequelize);
-        const globalModel = require("../models/sqlite/global.js")(sequelize);
+	const threadModel = require("../models/sqlite/thread.js")(sequelize);
+	const userModel = require("../models/sqlite/user.js")(sequelize);
+	const dashBoardModel = require("../models/sqlite/userDashBoard.js")(sequelize);
+	const globalModel = require("../models/sqlite/global.js")(sequelize);
 
-        await sequelize.sync({ force: false });
+	await sequelize.sync({ force: false });
 
-        return {
-            threadModel,
-            userModel,
-            dashBoardModel,
-            globalModel,
-            sequelize
-        };
-    } catch (e) {
-        // যদি মডেল ফাইলগুলো না থাকে তাও যেন বট ক্রাশ না করে
-        return { threadModel: {}, userModel: {}, dashBoardModel: {}, globalModel: {}, sequelize };
-    }
+	return {
+		threadModel,
+		userModel,
+		dashBoardModel,
+		globalModel,
+		sequelize
+	};
 };
