d3f6b235a619adda47bae49690f58c3f6ba12dc6
tawhidahmed4x <noobnahid19@gmail.com>
2026-03-03 06:43:20 +0600

    Update globalData.js

--- database/controller/globalData.js
+++ database/controller/globalData.js
@@ -1,19 +1,26 @@
-[
-  {
-    "key": "setalias",
-    "data": [],
-    "createdAt": "2026-03-02T07:11:35.093Z",
-    "updatedAt": "2026-03-02T07:11:35.093Z"
-  },
-  {
-    "key": "analytics",
-    "data": {
-      "ping": 2,
-      "pair": 1,
-      "help": 1,
-      "prefix": 1
-    },
-    "createdAt": "2026-03-02T07:22:08.580Z",
-    "updatedAt": "2026-03-02T09:57:38.394Z"
-  }
-]
+module.exports = async function () {
+    try {
+        return [
+            {
+                key: "setalias",
+                data: [],
+                createdAt: new Date().toISOString(),
+                updatedAt: new Date().toISOString()
+            },
+            {
+                key: "analytics",
+                data: {
+                    ping: 2,
+                    pair: 1,
+                    help: 1,
+                    prefix: 1
+                },
+                createdAt: new Date().toISOString(),
+                updatedAt: new Date().toISOString()
+            }
+        ];
+    } catch (err) {
+        console.log("[GLOBAL DATA ERROR]:", err.message);
+        return [];
+    }
+};
