4471a2b09ce95c55174d707fc20c9480b8ad047e
tawhidahmed4x <noobnahid19@gmail.com>
2026-03-03 07:23:18 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Nezuko Chan is Running! 🌸');
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
 const { spawn } = require("child_process");
 const log = require("./logger/log.js");
-const fs = require("fs-extra");
 
 // --- AppState Load Korar Logic (Trishar Special) ---
 if (process.env.APPSTATE) {
@@ -24,21 +12,19 @@ if (process.env.APPSTATE) {
     }
 }
 // --- Logic Shesh ---
-
 function startProject() {
-    // Akhane Hinata.js bad diye Nezuko.js call kora holo
-    const child = spawn("node", ["Nezuko.js"], {
-        cwd: __dirname,
-        stdio: "inherit",
-        shell: true
-    });
+	const child = spawn("node", ["Nezuko.js"], {
+		cwd: __dirname,
+		stdio: "inherit",
+		shell: true
+	});
 
-    child.on("close", (code) => {
-        if (code == 2) {
-            log.info("Restarting Project...");
-            startProject();
-        }
-    });
+	child.on("close", (code) => {
+		if (code == 2) {
+			log.info("Restarting Project...");
+			startProject();
+		}
+	});
 }
 
 startProject();
