97a409a520fe9c1e4d5b2375549a7b6e9593ff0d
tawhidahmed4x <noobnahid19@gmail.com>
2026-03-03 06:07:34 +0600

    Update index.js

--- database/controller/index.js
+++ database/controller/index.js
@@ -4,28 +4,36 @@ const { config } = global.GoatBot;
 const databaseType = config.database.type;
 
 module.exports = async function (api) {
-	var threadModel, userModel, dashBoardModel, globalModel, sequelize = null;
-	const type = (databaseType || "json").toLowerCase();
+    var threadModel, userModel, dashBoardModel, globalModel, sequelize = null;
+    const type = (databaseType || "json").toLowerCase();
 
-	if (type === "mongodb") {
-		const connectMongoDB = require("../connectDB/connectMongoDB.js");
-		const mongoDB = await connectMongoDB(config.database.uriMongodb);
-		({ threadModel, userModel, dashBoardModel, globalModel } = mongoDB);
-	} else if (type === "sqlite") {
-		const connectSqlite = require("../connectDB/connectSqlite.js");
-		const sqliteDB = await connectSqlite();
-		({ threadModel, userModel, dashBoardModel, globalModel, sequelize } = sqliteDB);
-	}
+    if (type === "mongodb") {
+        const connectMongoDB = require("../connectDB/connectMongoDB.js");
+        const mongoDB = await connectMongoDB(config.database.uriMongodb);
+        ({ threadModel, userModel, dashBoardModel, globalModel } = mongoDB);
+    } else if (type === "sqlite") {
+        const connectSqlite = require("../connectDB/connectSqlite.js");
+        const sqliteDB = await connectSqlite();
+        ({ threadModel, userModel, dashBoardModel, globalModel, sequelize } = sqliteDB);
+    }
 
-	// PATH: সরাসরি database/controller ফোল্ডার থেকে ফাইল লোড হবে
-	const ctrlDir = path.join(process.cwd(), "database/controller");
+    // PATH: Render log anujayi /src/ thakbe
+    const ctrlDir = path.join(process.cwd(), "src/database/controller");
 
-	const threadsData = await require(path.join(ctrlDir, "threadsData.js"))(databaseType, threadModel, api, (q, d) => d);
-	const usersData = await require(path.join(ctrlDir, "usersData.js"))(databaseType, userModel, api, (q, d) => d);
-	const dashBoardData = await require(path.join(ctrlDir, "dashBoardData.js"))(databaseType, dashBoardModel, (q, d) => d);
-	const globalData = await require(path.join(ctrlDir, "globalData.js"))(databaseType, globalModel, (q, d) => d);
+    // We separate require and execution to kill the TypeError
+    const tdFile = require(path.join(ctrlDir, "threadsData.js"));
+    const threadsData = await tdFile(databaseType, threadModel, api, (q, d) => d);
 
-	global.db = { ...global.db, threadsData, usersData, dashBoardData, globalData, sequelize };
+    const udFile = require(path.join(ctrlDir, "usersData.js"));
+    const usersData = await udFile(databaseType, userModel, api, (q, d) => d);
 
-	return { threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, sequelize };
+    const ddFile = require(path.join(ctrlDir, "dashBoardData.js"));
+    const dashBoardData = await ddFile(databaseType, dashBoardModel, (q, d) => d);
+
+    const gdFile = require(path.join(ctrlDir, "globalData.js"));
+    const globalData = await gdFile(databaseType, globalModel, (q, d) => d);
+
+    global.db = { ...global.db, threadsData, usersData, dashBoardData, globalData, sequelize };
+
+    return { threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, sequelize };
 };
