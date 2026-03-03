bdc1643f7074838bfce4d6416443c18cf346528a
tawhidahmed4x <noobnahid19@gmail.com>
2026-03-03 06:05:02 +0600

    Create loadData.js

---
+++ src/bot/login/loadData.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const { log, colors } = global.utils;
+
+module.exports = async function (api) {
+    console.log(colors.hex("#00FFFF")(" 🔄 [DATABASE] TawHid_Bbz, memories loading..."));
+    try {
+        const controllerPath = path.join(process.cwd(), 'src/database/controller/index.js');
+        const controller = require(controllerPath);
+        await controller(api);
+        console.log(colors.hex("#33FF33")(" ✅ [DATABASE] Data Loaded Successfully!"));
+    } catch (err) {
+        console.error("Critical Path Error:", err);
+        throw err;
+    }
+};
